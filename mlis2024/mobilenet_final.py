b04fd2eb21327a1be2a8168eac7829e032f3c74f
dhruv <144848933+dhruvsoni99@users.noreply.github.com>
2024-05-17 14:30:57 +0100

    Update mobilenet_final.py

--- mlis2024/mobilenet_final.py
+++ mlis2024/mobilenet_final.py
@@ -1,152 +1,240 @@
-import tensorflow as tf
 import os
-from datetime import datetime
-# from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+import datetime
+import random
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+
+import cv2
+from utils import get_merged_df
+
+import tensorflow as tf
+
+import keras
+from keras.models import Sequential, Model  
+from keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense, Input, GlobalAveragePooling2D
+from keras.models import load_model
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.layers import Input, Dense, Dropout, GlobalAveragePooling2D, Lambda, GaussianNoise
-from tensorflow.keras.models import Model
-from tensorflow.keras.metrics import Precision, Recall, F1Score
-from tensorflow.keras.callbacks import ModelCheckpoint
+
+from functools import partial
+import tensorflow as tf
+import keras.backend as K
+
+print( f'tf.__version__: {tf.__version__}' )
+print( f'keras.__version__: {keras.__version__}' )
+
+from PIL import Image
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
-        # Currently, memory growth needs to be the same across GPUs
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(f"{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs")
     except RuntimeError as e:
-        # Memory growth must be set before GPUs have been initialized
         print(e)
 
-tf.random.set_seed(123)
-# np.random.seed(123)
-def preprocess_image(image, label):
-    return tf.cast(image, tf.float32), label
+def my_imread(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
 
-# Model definition with preprocessing included
-def build_model(num_classes):
+def img_preprocess(image, label):
 
-    # with tf.device('/cpu:0'):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)  
+    image = cv2.resize(image, (192,192))
+    im = tf.image.convert_image_dtype(image, tf.float32) 
+    
+    return im, label
+
+
+def image_data_generator(image_paths, labels_dict, batch_size):
+    while True:
+        batch_images = []
+        batch_angles = []
+        batch_speeds = []
+        
+        for i in range(batch_size):
+            random_index = random.randint(0, len(image_paths) - 1)
+            image = my_imread(image_paths[random_index])
+
+            angle_label = labels_dict['angle_output'][random_index]
+            speed_label = labels_dict['speed_output'][random_index]
+              
+            image, angle_label = img_preprocess(image, angle_label)
+
+            angle_label = int(round(angle_label * 16)) 
+            batch_images.append(image)
+            
+            # do one hot category
+            angle_one_hot = to_categorical(angle_label, num_classes=17) 
+            batch_angles.append(angle_one_hot)
+            
+            # add speed label 
+            batch_speeds.append(speed_label)
+            
+        batch_angles = np.array(batch_angles)
+        batch_angles = batch_angles.reshape((batch_size, 17))
+        yield (np.asarray(batch_images), {'angle_output': batch_angles, 'speed_output': np.array(batch_speeds)})
+
+def mobile_net_classification_model():
+    inputs = Input(shape=(192, 192, 3))
+    
+    # data augmentation -> random brightness and contrast
     data_augmentation = tf.keras.Sequential([tf.keras.layers.RandomBrightness(0.1, seed=123),
                                              tf.keras.layers.RandomContrast(0.1, seed=123),
-                                            tf.keras.layers.RandomFlip('horizontal', seed=123),
-                                            # tf.keras.layers.CenterCrop(224,224),
-                                            # tf.keras.layers.RandomZoom(0.1, fill_mode='reflect', seed=123),
-                                            # tf.keras.layers.RandomRotation(0.05, fill_mode='nearest', seed=123),
 
     ])
 
-    base_model = tf.keras.applications.MobileNetV2(input_shape=(160, 160, 3), include_top=False, weights='imagenet')
-    base_model.trainable = False  # Freeze base model
-
-    inputs = Input(shape=(160,160,3))
-    aug_inputs = data_augmentation(inputs)
-    x = preprocess_input(aug_inputs)  # Preprocessing for MobileNetV2
-    x = base_model(x, training=False)
-    x = GlobalAveragePooling2D()(x)
-    x = Dropout(0.3)(x)
-    # x = Dense(1024, activation='relu')(x)
-    # x = Dropout(0.35)(x)
-    outputs = Dense(num_classes, activation='softmax')(x)
+    # mobilenetv2 base model
+    base_model = tf.keras.applications.MobileNetV2(include_top=False, weights="imagenet", input_tensor=inputs)
+    base_model.trainable = False
     
-    model = Model(inputs, outputs)
+    aug_inputs = data_augmentation(inputs)
 
+    x = base_model(aug_inputs, training=False)
+    
+    # global average pooling
+    x = GlobalAveragePooling2D()(x)
+    
+    # 52 unit dense layer
+    x = Dense(52, activation='relu')(x)
+    
+    # 30% dropout rate
+    x = Dropout(0.3)(x)
 
-    # optimizer = RMSprop(learning_rate=0.00001)  # Lower learning rate
-    optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=0.001)
+    # angle prediction
+    angle_output = Dense(17, activation='softmax', name='angle_output')(x)
+
+    # speed prediction
+    speed_output = Dense(1, activation='sigmoid', name='speed_output')(x) 
+
+    model = Model(inputs=inputs, outputs=[angle_output, speed_output])
+   
+    # higher learning rate for initial training
+    custom_lr = 0.001  
+    optimizer = Adam(learning_rate=custom_lr)
 
     model.compile(optimizer=optimizer,
-                  loss='categorical_focal_crossentropy',
-                  metrics=['accuracy', F1Score(), tf.metrics.MeanSquaredError()])
+                  loss={'angle_output': 'categorical_crossentropy', 'speed_output': 'binary_crossentropy'},
+                  metrics={'angle_output': 'accuracy', 'speed_output': 'accuracy'})
+
     return model
 
-# Fine-tuning function
-def fine_tune_model(model):
-    # Unfreeze all layers in base model
-    print("Finetuning ...")
+def predict(self, image):
+        angles = np.arange(17)*5+50
+  
+        image = self.preprocess(image)
+        
+        pred_speed = self.combined_model.predict(image)[1][0][0]
+        
+        if pred_speed < 0.5:
+            pred_speed = 0
+        else:
+            pred_speed = 1
 
+        speed = pred_speed * 35
 
-    model.layers[4].trainable = True
-    for layer in model.layers[4].layers[:-5]:
-        layer.trainable = False
-    for layer in model.layers[4].layers[:]:
-        if isinstance(layer, tf.keras.layers.BatchNormalization):
-            layer.trainable = False
+        pred_angle = self.combined_model.predict(image)[0][0]
+        angle = np.argmax(pred_angle)
+    
+        predicted_angle = angles[angle]
 
-    # optimizer = RMSprop(learning_rate=0.00001)  # Lower learning rate
-    optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=0.00001)
-    # Recompile the model with a lower learning rate
-    model.compile(optimizer=optimizer,
-                  loss='categorical_focal_crossentropy',
-                  metrics=['accuracy', F1Score(), tf.metrics.MeanSquaredError()])
-    model.summary()
+        print('angle:', angle,'speed:', speed)
+        return predicted_angle, speed
+    
 
-finetuning = True
+data_dir = 'training_data/training_data'
+norm_csv_path = 'training_norm.csv'
+cleaned_df = get_merged_df(data_dir, norm_csv_path)
 
-if __name__ == "__main__":
-    directory = 'angle_class_data'
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-                directory,
-                labels='inferred',
-                label_mode='categorical',
-                color_mode='rgb',
-                batch_size=64,
-                image_size = (160,160),
-                shuffle=True,
-                seed=123,
-                validation_split=0.2,
-                subset="training").map(preprocess_image)
+print(cleaned_df)
 
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-                directory,
-                labels='inferred',
-                label_mode='categorical',
-                color_mode='rgb',
-                batch_size=32,
-                image_size = (160,160),
-                shuffle=True,
-                seed=123,
-                validation_split=0.2,
-                subset="validation").map(preprocess_image)
+angle_labels = cleaned_df['angle'].to_list()
+speed_labels = cleaned_df['speed'].to_list()
+image_paths = cleaned_df['image_path'].to_list()
 
-    ts = datetime.now().strftime('%Y%m%d')
-    checkpoint_path = f"models/angle/mobilenet_v1_{ts}"
+X_train, X_valid, angle_train, angle_valid, speed_train, speed_valid = train_test_split(image_paths, angle_labels, speed_labels, test_size=0.3)
 
-    model_checkpoint_callback = ModelCheckpoint(
-    checkpoint_path,
-    monitor='val_loss',
+model = mobile_net_classification_model()
+
+print(model.summary())
+
+model_output_dir = 'inpersonmodels/'  
+
+# clean up log folder for tensorboard
+log_dir_root = f'{model_output_dir}/logs'
+
+tensorboard_callback = TensorBoard(log_dir_root, histogram_freq=1)
+
+# path to save model
+tfmodelpath = '/home/psysm13/GOAT-3/autopilot/models/two_epoch_mobilenet/mobilenetearly/'
+
+# early stopping in case the val loss does not improve
+early_stopping = EarlyStopping(
+monitor='val_loss',  
+patience=10,         
+verbose=1,           
+restore_best_weights=True  
+)
+
+# create a checkpoint callback
+model_checkpoint_callback = ModelCheckpoint(
+    tfmodelpath,
+    monitor='val_loss',     # monitor val loss
+    verbose=10,              
+    save_best_only=True,    
+    save_weights_only=False, 
+    mode='min',             
+    save_freq='epoch',
+    save_format='tf'      
+    ) 
+
+print(model.summary())
+
+history = model.fit(
+    image_data_generator(X_train, {'angle_output': angle_train, 'speed_output': speed_train}, batch_size=128),
+    steps_per_epoch=len(X_train) // 128,
+    epochs=10,
+    validation_data = image_data_generator(X_valid, {'angle_output': angle_valid, 'speed_output': speed_valid}, batch_size=128),
+    validation_steps=len(X_valid) // 128,
     verbose=1,
-    save_best_only=True,
-    save_weights_only=False,
-    mode='min',
-    save_freq='epoch')
+    callbacks=[early_stopping, model_checkpoint_callback, tensorboard_callback]
+    )
 
-    if not finetuning:
-        print("Base Learning")
-        model = build_model(17)
-        model.summary()
+# unfreeze top layers
+for layer in model.layers[-20:]:
+    layer.trainable = True
 
-        history = model.fit(train_ds, epochs=10, validation_data=val_ds, callbacks=[model_checkpoint_callback], verbose=1)
+fine_tuning_learning_rate = 0.0001  
+fine_tune_optimizer = Adam(learning_rate=fine_tuning_learning_rate)
 
-    # Load the best model for fine-tuning
-    model = tf.keras.models.load_model(checkpoint_path)
+model.compile(optimizer= fine_tune_optimizer,
+    loss={'angle_output': 'categorical_crossentropy', 'speed_output': 'binary_crossentropy'},
+    metrics={'angle_output': 'accuracy', 'speed_output': 'accuracy'})
 
-    # Fine-tuning
-    fine_tune_model(model)
-    # Fine-tuning training with a smaller learning rate
-    fine_tune_epochs = 10
-    total_epochs = 10 + fine_tune_epochs  # Initial epochs + fine-tune epochs
+# fine tune model 
+history_fine = model.fit(
+    image_data_generator(X_train, {'angle_output': angle_train, 'speed_output': speed_train}, batch_size=128),
+    steps_per_epoch=len(X_train) // 128,
+    epochs=10,  
+    validation_data = image_data_generator(X_valid, {'angle_output': angle_valid, 'speed_output': speed_valid}, batch_size=128),
+    validation_steps=len(X_valid) // 128,
+    verbose=1,
+    callbacks=[model_checkpoint_callback]
+    )
 
-    history_finetune = model.fit(
-                            train_ds,
-                            validation_data=val_ds,
-                            epochs=total_epochs,
-                            callbacks=[model_checkpoint_callback],
-                            verbose=1)
-
-    # # Evaluate the fine-tuned model
-    # evaluate_model(model, valid_generator)
+model.save('/home/psysm13/GOAT-3/autopilot/models/two_epoch_mobilenet/finalmodel_resc_noflip/', save_format='tf')
